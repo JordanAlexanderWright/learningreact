c99c943ad38ff84a3400c03a9a49f65c587ddb0c
Jordan Wright <jaws610@gmail.com>
2022-08-15 11:00:00 -0400

    Converting function component to a class component, and instantiating state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+// Converting to a class component
+import {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//This has the same functionality as before, but it was converte to a class
+// COmponent already has the render() method, and it palces what I want to render inside of it. The JSX. 
+class App extends Component {
+
+  //Inside of the constructor, call super to inherit from the parent class, then instantiate the state. 
+  constructor(){
+    super();
+
+    this.state ={
+        name: 'Jordan Wright'
+    }
+  }
+
+  render() {
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello my name is {this.state.name}
+          </p>
+          <button>Click Me</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
